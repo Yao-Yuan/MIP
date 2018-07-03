3229a6891a115c0728c709026a2b5ba9210f6cfd
Yao-Yuan <yao_yuan_@outlook.com>
2018-07-03 13:07:57 +0200

    Change last activation function to tanh

--- 3dnet/net3d_model.py
+++ 3dnet/net3d_model.py
@@ -48,7 +48,7 @@ def dice_coef(y_true, y_pred):
     return (2. * intersection + smooth) / (backend.sum(y_true_f) + backend.sum(y_pred_f) + smooth)
 
 def dice_coef_loss(y_true, y_pred):
-    return -dice_coef(y_true, y_pred)
+    return 1.-dice_coef(y_true, y_pred)
 
 SIZE = 256
 
